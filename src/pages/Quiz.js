13e880690c79e5a6ef2f00e1d20f641503f78668
Jack Brooker <95284611+Bos116@users.noreply.github.com>
2025-01-16 19:52:29 +0000

    Feat: added quiz page, started interactive quiz

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import logo from '../components/images/logo.svg'; // Adjust the path if necessary
+import ClickableImage from '../components/quiz/ClickableImage'; // Import the ClickableImage component
+import '../App.css'; // Import the CSS file
 
 const Quiz = () => {
   return (
-    <div>Quiz</div>
-  )
-}
+    <div className="quiz-container">
+      <h1 className="quiz-title">Quiz</h1>
+      <ClickableImage 
+        className="quiz-image"
+        src={logo} 
+        alt="Quiz Logo" 
+      />
+      <div>
+        <p>
+          Explore the image and click on an engineering field that sparks your curiosity and excitement!
+        </p>
+      </div>
 
-export default Quiz
+      <div className="quiz-ap">
+        <button className="quiz-button">Next Question</button>
+        <button className="quiz-button">Hint</button>
+        <button className="quiz-button">Previous Question</button>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
