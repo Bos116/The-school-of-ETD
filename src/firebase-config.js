959e6d0b2266779f8833f83d51f716679c3f70dc
Jack Brooker <95284611+Bos116@users.noreply.github.com>
2025-02-08 22:34:01 +0000

    feat: hidden sesitive information surrounding firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCzkuB1ljxuVeHRTgDsKmwY5y8-iAcm6Jw",
-  authDomain: "blog-app-b346d.firebaseapp.com",
-  projectId: "blog-app-b346d",
-  storageBucket: "blog-app-b346d.firebasestorage.app",
-  messagingSenderId: "990475489108",
-  appId: "1:990475489108:web:c88fa9f4548b72b03242c1"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSENGER_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase and Database, Auth, and googleauth provider
